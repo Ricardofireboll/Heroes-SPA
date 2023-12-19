fe7d0b0989381c3fecf5ac044cbc528f49083a08
Ricardo <ricardofirebol@gmail.com>
2023-12-19 01:25:39 -0600

    Actualizacion de carpeta docs

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -20,8 +20,8 @@ export const HeroCard = ({
 }) => {
   return (
     <>
-        <Link to={`https://ricardofireboll.github.io/Heroes-SPA/heroes/${id}`} className="my-card animate__animated animate__fadeIn">
-            <img src={`/heroes/${id}.jpg`} className="" alt={superhero}/>
+        <Link to={`/hero/${id}`} className="my-card animate__animated animate__fadeIn">
+            <img src={`https://ricardofireboll.github.io/Heroes-SPA/heroes/${id}.jpg`} className="" alt={superhero}/>
             <div className="profile-name">{superhero}</div>
             <div className="profile-position">{alter_ego}</div>
             <div className="profile-overview">
